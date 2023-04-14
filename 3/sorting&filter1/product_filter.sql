24de556bc54b60340a5e57bd847766ea0402274c
Ali Kashi <92630617+ali-kashi@users.noreply.github.com>
2023-04-14 23:10:24 +0330

    Delete 3/sorting&filter1 directory

--- 3/sorting&filter1/product_filter.sql
+++
@@ -1,8 +0,0 @@
-//فیلتر کردن بر اساس نام
-SELECT * FROM `product` WHERE name LIKE "%موبایل%"
-
-//فیلتر کردن بر اساس قیمت
-SELECT * FROM `product` WHERE price < 5000000
-
-//فیلتر کردن بر اساس وضعیت آن
-SELECT * FROM `product` WHERE status = "active"
--- 3/sorting&filter1/product_sort.sql
+++
@@ -1,7 +0,0 @@
-//مرتب سازی بر اساس تازه ترین محصول
-SELECT * FROM `product` 
-order by id DESC
-
-//مرتب سازی بر اساس ارزان ترین محصول
-SELECT * FROM `product` 
-order by price
